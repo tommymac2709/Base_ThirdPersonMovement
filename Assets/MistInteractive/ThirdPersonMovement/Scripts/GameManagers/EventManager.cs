659bfc6c1f0900b44cadf8d16ea5bba2cd6ff309
tommymac2709 <tom.mckenzie@outlook.com>
2025-11-04 21:44:29 +1100

    Add EventManager

---
+++ Assets/MistInteractive/ThirdPersonMovement/Scripts/GameManagers/EventManager.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+
+namespace MaintenanceCrew.Core
+{
+    public static class EventManager
+    {
+        private static readonly Dictionary<string, Delegate> events = new Dictionary<string, Delegate>();
+        private static readonly object lockObj = new object();
+
+        public static void StartListening(string eventName, Action listener)
+        {
+            lock (lockObj)
+            {
+                if (events.TryGetValue(eventName, out var existing))
+                {
+                    events[eventName] = Delegate.Combine(existing, listener);
+                }
+                else
+                {
+                    events.Add(eventName, listener);
+                }
+            }
+        }
+
+        public static void StopListening(string eventName, Action listener)
+        {
+            lock (lockObj)
+            {
+                if (events.TryGetValue(eventName, out var existing))
+                {
+                    var updated = Delegate.Remove(existing, listener);
+                    if (updated == null)
+                        events.Remove(eventName);
+                    else
+                        events[eventName] = updated;
+                }
+            }
+        }
+
+        public static void TriggerEvent(string eventName)
+        {
+            Delegate del;
+            lock (lockObj)
+            {
+                events.TryGetValue(eventName, out del);
+            }
+            (del as Action)?.Invoke();
+        }
+
+        public static void StartListening<T>(string eventName, Action<T> listener)
+        {
+            lock (lockObj)
+            {
+                if (events.TryGetValue(eventName, out var existing))
+                {
+                    events[eventName] = Delegate.Combine(existing, listener);
+                }
+                else
+                {
+                    events.Add(eventName, listener);
+                }
+            }
+        }
+
+        public static void StopListening<T>(string eventName, Action<T> listener)
+        {
+            lock (lockObj)
+            {
+                if (events.TryGetValue(eventName, out var existing))
+                {
+                    var updated = Delegate.Remove(existing, listener);
+                    if (updated == null)
+                        events.Remove(eventName);
+                    else
+                        events[eventName] = updated;
+                }
+            }
+        }
+
+        public static void TriggerEvent<T>(string eventName, T param)
+        {
+            Delegate del;
+            lock (lockObj)
+            {
+                events.TryGetValue(eventName, out del);
+            }
+            (del as Action<T>)?.Invoke(param);
+        }
+
+        public static void ClearAllListeners()
+        {
+            lock (lockObj)
+            {
+                events.Clear();
+            }
+        }
+    }
+}
